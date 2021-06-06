46f02256d140b75bb2fc7be13c426ba8ac4969dc
altadzk <71278831+altadzk@users.noreply.github.com>
2021-06-06 23:18:58 +0700

    Add files via upload

---
+++ pertemuan 13/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host		="localhost";
+$username	="root";
+$password	="";
+$database	="pertemuan13";
+$koneksi	=mysqli_connect($host, $username, $password, $database);
+
+if (mysqli_connect_errno()) {
+	echo "Koneksi database gagal : " . mysql_connect_error();
+}
+
+?>
